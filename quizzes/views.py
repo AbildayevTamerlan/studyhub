1a006cf51bb09d089af65d51c42dbe2c34c35f2b
AbildayevTamerlan <tamerlanabildayev@gmail.com>
2026-07-04 19:08:21 +0500

    add user authentication

--- quizzes/views.py
+++ quizzes/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 
-from quizzes.models import Subject, Test
+from quizzes.models import Subject, Test, Answer
 
 
 def home(request):
-   return render(request, "home.html")
+    return render(request, "home.html")
+
 
 def subjects(request):
     context = {"subjects": Subject.objects.all()}
@@ -12,23 +13,27 @@ def subjects(request):
 
 
 def subject_tests(request, subject_id):
-   subject = get_object_or_404(Subject, pk=subject_id)
-   context = {
-      "subject": subject,
-      "tests": subject.tests.all()
-      }
-   return render(request, "subject_tests.html", context)
+    subject = get_object_or_404(Subject, pk=subject_id)
+    context = {"subject": subject, "tests": subject.tests.all()}
+    return render(request, "subject_tests.html", context)
+
 
 def take_test(request, test_id):
-   test = get_object_or_404(Test, pk=test_id)
-   context = {
-      "test": test,
-      "questions": test.questions.all()
-   }
-   return render(request, "take_test.html", context)
+    test = get_object_or_404(Test, pk=test_id)
+    if request.method == "POST":
+        score = 0
+        for question in request.POST:
+            if question.startswith("question_"):
+                selected_answer = request.POST[question]
+                answer = Answer.objects.get(pk=selected_answer)
+                if answer.is_correct:
+                    score += 1
+        return render(request, "test_result.html", {"test": test, "score": score})
+    else:
+        context = {"test": test, "questions": test.questions.all()}
+        return render(request, "take_test.html", context)
+
 
 def test_result(request, test_id):
-   context = {
-      "test": get_object_or_404(Test, pk=test_id)
-   }
-   return render(request, "test_result.html", context)
+    context = {"test": get_object_or_404(Test, pk=test_id)}
+    return render(request, "test_result.html", context)
